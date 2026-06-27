2f1aed9eab8fc8a5509171256a95b0626ac940c3
montmorill <montmorill@outlook.com>
2026-06-28 02:04:26 +0800

    refactor: update package name and version, enhance index.ts functionality, and add ESLint configuration

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { Context, Schema } from 'koishi'
+import type { Context, Session } from 'koishi'
+import { h, Schema } from 'koishi'
 
 export const name = 'welcome'
 
@@ -6,6 +7,29 @@ export interface Config {}
 
 export const Config: Schema<Config> = Schema.object({})
 
-export function apply(ctx: Context, config: Config) {
-  // write your plugin here
+export function apply(ctx: Context) {
+  ctx.i18n.define('zh-CN', {
+    added: '{at}，欢迎加入群聊！',
+    removed: '{username}退出了群聊。',
+  })
+
+  function makeParam(session: Session): object {
+    return {
+      userId: session.userId,
+      username: session.username,
+      user: session.user,
+      channel: session.channel,
+      guild: session.guild,
+      at: h.at(session.userId),
+    }
+  }
+
+  ctx.on('guild-member-added', async (session) => {
+    const text = session?.text('added', makeParam(session))
+    await session.send(text)
+  })
+
+  ctx.on('guild-member-removed', async (session) => {
+    await session.send(session.text('removed', session))
+  })
 }
